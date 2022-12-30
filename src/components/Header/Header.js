558284a95f8918dc8b537c6be20556833ea3748b
Satyam Pund <pundsatyam@gmail.com>
2022-12-31 03:57:22 +0530

    feat: mobille friendly header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,16 @@ const Header = () => {
     <>
       <div class="container text-center">
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-md-6 order-2 order-md-1">
             <h1 className="heading">Hey, I'm Satyam Pund</h1>
-            <h4 className="heading-description mt-4 mb-3">
+            <h4 className="heading-description mt-md-4 mb-md-3">
               A MERN Stack Developer Passionate About Coding. Always ready to try Hands-on New and
               Emerging Technologies.
             </h4>
             <button className="css-button-arrow--sky me-5 mt-4">Resume</button>
             <button className="css-button-sliding-to-left--sky mt-4">Hire Me</button>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-6 order-1 order-md-2">
             <img src={StudyBoy} className="header-img" alt="study"></img>
           </div>
         </div>
